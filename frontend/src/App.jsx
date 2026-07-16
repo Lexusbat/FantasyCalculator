c2b9bc8b26687768e37fa1aec2a31a587ebcf9d2
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-17 00:13:41 +0200

    UI positioning and debugging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,22 +7,21 @@ import './index.css'
 function App() {
 
   return (
-    <div>
-      <div className="flex flex-row  space-x-4 ">
-        <div className= " w-20 h-20 bg-blue-500 rounded-md ">01</div>
-        <div className= " w-20 h-20 bg-blue-500 rounded-md">02</div>
-        <div className= " w-20 h-20 bg-blue-500 rounded-md">03</div>
+    <div className=" m-20">
+      <div className="flex flex-row  space-x-4 space-y-4 flex justify-center ">
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">01</div>
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center">02</div>
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center">03</div>
       </div>
-      <div className="flex flex-row  space-x-4 ">
-        <div className= " w-20 h-20 bg-blue-500 rounded-md ">04</div>
-        <div className= " w-20 h-20 bg-blue-500 rounded-md ">05</div>
-        <div className= " w-20 h-20 bg-blue-500 rounded-md ">06</div>
-
+      <div className="flex flex-row  space-x-4 space-y-4 flex justify-center ">
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">04</div>
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center">05</div>
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center">06</div>
       </div>
-      <div className="flex flex-row  space-x-4 ">
-        <div className= " w-20 h-20 bg-blue-500 rounded-md ">07</div>
-        <div className= " w-20 h-20 bg-blue-500 rounded-md ">08</div>
-        <div className= " w-20 h-20                                                    bg-blue-500 rounded-md ">09</div>
+      <div className="flex flex-row  space-x-4 space-y-4 flex justify-center ">
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">07</div>
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">08</div>
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">09</div>
       </div>
     </div>
   )
