1df819abab44c676e478e75faefa412889d20b37
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-16 19:59:49 +0200

    Tailwind redownload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+  <div className="bg-red-500 text-white p-4">
+   Test
+  </div>
   )
 }
 
