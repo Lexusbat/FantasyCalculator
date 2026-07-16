015ed31ce5065183f4d3cfe4bac3fb8aefa57f1e
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-17 00:24:50 +0200

    Button experimentation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,15 @@ import heroImg from './assets/hero.png'
 import './index.css'
 
 function App() {
+  const handleClick = () => {
+    alert("Button was clicked!");
+  };
 
   return (
     <div className=" m-20">
       <div className="flex flex-row  space-x-4 space-y-4 flex justify-center ">
-        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">01</div>
+        <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">
+          <button>click me</button></div>
         <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center">02</div>
         <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center">03</div>
       </div>
@@ -23,6 +27,9 @@ function App() {
         <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">08</div>
         <div className=" w-20 h-20 bg-blue-500 rounded-md flex justify-center items-center ">09</div>
       </div>
+      <button onClick={handleClick}>
+        Click Me
+      </button>
     </div>
   )
 }
