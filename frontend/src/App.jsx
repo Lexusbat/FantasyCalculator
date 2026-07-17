9cb86b4be9bf356f08c8d890c165ca79fad7b049
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-17 18:46:52 +0200

    Additional image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Calculator from "./components/calculator";
-
+import Mascot from "./assets/mascot2.png";
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Calculator />
+      <div className="w-150 h-150">
+        <img className="" src= {Mascot} alt="Mascot" />
+      </div>
+      
     </div>
   )
 }
