0ead44c9f88d7f2584b7c0bff1fc279afb1241d5
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-17 22:24:55 +0200

    Font Change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="bg-blue-950 ">
           <div className="bg-white rounded-full">
             <img className="animate-bounce flex justify-start" src={Mascot} alt="Mascot" />
-                        <h1 className="flex items-center justify-center text-lg absolute inset-x-0 top-25">Calculator</h1>
+                        <h1 className="flex items-center justify-center fondamento-regular text-9xl absolute inset-x-0 top-20">Calculator</h1>
           </div>
     </div>
       <div className="bg-blue-950 min-h-screen flex items-center justify-center  ">
