667077a369c15e36f9f4894af2055952a36384d3
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-17 14:23:06 +0200

    ButtonGrid File code

---
+++ frontend/src/components/buttongrid.jsx
@@ -0,0 +1,20 @@
+import Buttons from './buttons'
+
+function ButtonGrid({ display }, { setdisplay }) {
+return 
+<>
+<button>
+    value = "1"
+    display = {display}
+    setdisplay = {setdisplay}
+</button>
+
+<button>
+    value = "1"
+    display = {display}
+    setdisplay = {setdisplay}
+</button>
+
+</>
+}
+export default ButtonGrid
