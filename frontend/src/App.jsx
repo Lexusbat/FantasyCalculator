4a0ee0ae9d60eefae2d0e5e4eb5d40b84859fd4b
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-17 19:15:26 +0200

    UI Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <> 
     <div className="bg-blue-950 ">
-          <div className="bg-white rounded-full flex justify-start">
-            <img className="animate-bounce" src={Mascot} alt="Mascot" />
+          <div className="bg-white rounded-full">
+            <img className="animate-bounce flex justify-start" src={Mascot} alt="Mascot" />
+                        <h1 className="flex items-center justify-center">Hello</h1>
           </div>
     </div>
       <div className="bg-blue-950 min-h-screen flex items-center justify-center  ">
